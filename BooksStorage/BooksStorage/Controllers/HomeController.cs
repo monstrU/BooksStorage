c56ee230279604f804dc1a469ff74654d1f9bfbc
monstrU <hp1999@mail.ru>
2016-02-01 23:13:43 +0300

    подключен провайдер для загрузки данных

--- BooksStorage/BooksStorage/Controllers/HomeController.cs
+++ BooksStorage/BooksStorage/Controllers/HomeController.cs
@@ -1,11 +1,25 @@
 ﻿using System.Web.Mvc;
+using FacadeServices;
+using FacadeServices.Factories;
+using FacadeServices.Interfaces;
 
 namespace BooksStorage.Controllers
 {
     public class HomeController : Controller
     {
+        public IBookStorageService DataService { get; set; }
+        public IDataProvider DataProvider { get; set; }
+
+        public HomeController()
+        {
+            var dbFactory = new SqLiteConnectionFactory();
+            DataProvider = new DataProvider(dbFactory);
+            DataService = new BookStorageService(DataProvider);
+            DataService.InitSqDb();
+        }
         public ActionResult Index()
         {
+            var books= DataService.LoadBooks();
             return View();
         }
     }
